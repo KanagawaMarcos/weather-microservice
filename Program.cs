1bf0c9095654540cb377f9a9a231a6f229e9e427
Marcos Costa Santos <msantos@ceapema.matriz>
2018-07-09 12:00:51 -0300

    project restarted

--- Program.cs
+++ Program.cs
@@ -1,58 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-namespace linq
+namespace WeatherMicroservice
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-		
-	            var startingDeck =  from s in Suits()
-        	                        from r in Ranks()
-                	                select new { Suit = s, Rank = r };
-           	    var shuffle = startingDeck;
-		    var times = 0;
-		do
-		{
-		    shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
-
-	            foreach (var c in shuffle)
-        	    {
-                	Console.WriteLine(c);
-            	    }
-
-		    Console.WriteLine();
-		    times++;
-		}while(!startingDeck.SequenceEquals(shuffle));
-
-		Console.WriteLine(times);
-        }
-        
-        static IEnumerable<string> Suits()
-        {
-            yield return "clubs";
-            yield return "diamonds";
-            yield return "hearts";
-            yield return "spades";
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        static IEnumerable<string> Ranks()
-        {
-            yield return "two";
-            yield return "three";
-            yield return "four";
-            yield return "five";
-            yield return "six";
-            yield return "seven";
-            yield return "eight";
-            yield return "nine";
-            yield return "ten";
-            yield return "jack";
-            yield return "queen";
-            yield return "king";
-            yield return "ace";
-        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
